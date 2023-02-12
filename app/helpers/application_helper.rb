4e76ab6dbb99834857db62bcf289d89ae3356528
Edward Bako <edward.bako@gmail.com>
2023-02-13 00:14:15 +0700

    Remove flash classes mapper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,2 @@
 module ApplicationHelper
-
-  def flash_class(name)
-    "alert-#{flash_mapping[name]}"
-  end
-
-  def flash_mapping
-    map = Hash.new(:info)
-    map.merge( {
-      notice: :success,
-      alert: :warning,
-      error: :danger
-    }).with_indifferent_access
-  end
-
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
+import "ruby-head-wasm-wasi/dist/browser.script.iife"
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
