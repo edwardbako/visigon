7a254db084b58d2e026f6bd2628134fa0f704fff
Edward Bako <edward.bako@gmail.com>
2023-02-16 16:30:32 +0700

    Split drawer to browser and console work && Add loading indicator

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -4,23 +4,20 @@ import { ready } from "../services/rubyVM"
 
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
+
 x = 250
 y = 250
 
 start = 0
 step = Math.PI / 60
-
-let loading
-
 upe = function (timstamp) {
   start = start + step
-  stop = start + 2 / 5 * Math.PI
+  stop = start + 3 / 5 * Math.PI + Math.sin(start)
+
 
   ctx.clearRect(0, 0, 500, 500)
   ctx.beginPath()
   ctx.arc(x, y, 20, start, stop, true)
-  // ctx.fillStyle = "black"
-  // ctx.fill()
   ctx.strokeStyle = "black"
   ctx.stroke()
 
